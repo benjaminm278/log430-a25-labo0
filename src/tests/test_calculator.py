6176998c7b3d7fc80b01a5213d0176d5c9b85007
Benjamin Mah <benjamin.mah.1@ens.etsmtl.ca>
2025-09-10 19:32:53 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -8,6 +8,9 @@ from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
 # TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    assert Calculator.addition(my_calculator, 2, 3) == 5
