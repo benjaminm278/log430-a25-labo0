cd6b9ce1c2dbecb84486b6bf0a0220f5af4fbcc2
Benjamin Mah <benjamin.mah.1@ens.etsmtl.ca>
2025-09-11 14:30:10 -0400

    Add new multiplication test

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -8,9 +8,14 @@ from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
+    assert my_calculator.get_hello_message() == "Calculatrice"
 
 # TODO: ajoutez les tests
 def test_addition():
     my_calculator = Calculator()
     assert Calculator.addition(my_calculator, 2, 3) == 5
+
+
+def test_multiplication():
+    my_calculator = Calculator()
+    assert Calculator.multiplication(my_calculator, 5, 3) == 5
